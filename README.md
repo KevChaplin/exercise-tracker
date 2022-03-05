c24539a47ab9c65d4aab7c17246b794654202d96
KevChaplin <kevchaplin44@gmail.com>
2022-03-05 18:07:34 +0900

    routes bare bones added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# Exercise Tracker
+
+## Project 4 of the Free Code Camp's Back End Development and APIs Course
+
 # [Exercise Tracker](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker)
--- server.js
+++ server.js
@@ -5,9 +5,31 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+
+// Routes
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+app.post('/api/users', (req, res) => {
+  console.log(req.body)
+  res.send('Post new user')
+})
+
+app.get('/api/users', (req, res) => {
+  res.send('Get all users')
+})
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+  res.send('Post user workout')
+})
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  res.send('Get user workout logs')
+})
+
+
 
 
 
