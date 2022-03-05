558d13a2b6da9c61e29de0a3a4d8d05dbf3a17e1
KevChaplin <kevchaplin44@gmail.com>
2022-03-05 19:35:57 +0900

    post new user route functional

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
+// Connect DB
+const connectDB = (url) => {
+  return mongoose.connect(url)
+}
+
+// Schema
+const userSchema = new Schema ({
+  username: String,
+})
+const User = mongoose.model('User', userSchema)
+
+// Middleware
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
 
 // Routes
 
@@ -12,9 +28,18 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.post('/api/users', (req, res) => {
-  console.log(req.body)
-  res.send('Post new user')
+app.post('/api/users', async (req, res) => {
+  try {
+    const userObj = {
+      username: req.body.username
+    }
+    const newUser = new User(userObj)
+    await newUser.save()
+    return res.json(newUser)
+  } catch (err) {
+    console.log(err)
+    return res.json({err: 'Cannot create user, please try again later.'})
+  }
 })
 
 app.get('/api/users', (req, res) => {
@@ -30,11 +55,19 @@ app.get('/api/users/:_id/logs', (req, res) => {
 })
 
 
+// Basic Configuration
+const port = process.env.PORT || 3000
 
+// Run App
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => 
+      console.log(`Server is listening on port ${port}...`)
+    )
+  } catch (err) {
+    console.log(err)
+  }
+}
 
-
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+start()
